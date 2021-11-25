35b54e77e7afffe8f7e61c31bc5492063f205138
Alex Thon <ghaiquol@gmail.com>
2021-11-25 03:59:35 +0000

    Refactor name_of_teachers_who_assisted.sql

--- 4_queries/name_of_teachers_who_assisted.sql
+++ 4_queries/name_of_teachers_who_assisted.sql
@@ -4,4 +4,4 @@ JOIN assistance_requests on teacher_id = teachers.id
 JOIN students on students.id = student_id
 JOIN cohorts on cohorts.id = cohort_id
 WHERE cohorts.name = 'JUL02'
-GROUP BY teachers.name, cohorts.name
+GROUP BY teacher;
