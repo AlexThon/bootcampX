8472eb1cd9239d6fdcb3cf09d17cfbe86f91c8ce
Alex Thon <ghaiquol@gmail.com>
2021-11-25 04:11:04 +0000

    Add name_of_teachers_and_assistances.sql

---
+++ 4_queries/name_of_teachers_and_assistances.sql
@@ -0,0 +1,9 @@
+SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.*) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+
+GROUP BY teachers.name, cohorts.name
+HAVING cohorts.name = 'JUL02'
+ORDER BY teacher;
